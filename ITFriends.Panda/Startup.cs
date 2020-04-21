7eb554f84e33d08e3df4ad0d1f91bf8ff8567c2e
Vadym <VadimircychDnepr@gmail.com>
2020-04-20 21:58:29 -0400

    Removed initial database seeding until better defined for the Panda project

--- ITFriends.Panda/Startup.cs
+++ ITFriends.Panda/Startup.cs
@@ -10,42 +10,15 @@ namespace ITFriends.Panda
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-        public IHostingEnvironment Environment { get; }
-        public string ConnectionString { get; set; }
-
-        public Startup(IConfiguration configuration, IHostingEnvironment environment)
-        {
-            Configuration = configuration;
-            Environment = environment;
-            if (environment.IsProduction())
-            {
-                ConnectionString = Configuration.GetConnectionString("ProductionConnection");
-            }
-            else
-            {
-                ConnectionString = Configuration.GetConnectionString("Default");
-            }
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<PandaDbContext>(options =>
-            //    options.UseSqlServer(ConnectionString));
-
-            //services.AddIdentity<ApplicationUser, ApplicationRole>()
-            //    .AddEntityFrameworkStores<PandaDbContext>();
-
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-            //PandaDbContext context,
-            //RoleManager<ApplicationRole> roleManager,
-            //UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -61,7 +34,6 @@ namespace ITFriends.Panda
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            //InitialData.Seed(context, userManager, roleManager, Configuration).Wait();
         }
     }
 }
